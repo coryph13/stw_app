81d250074e63a7d58b8a39d451337d951122446e
Coryph <coryph13@gmail.com>
2024-08-23 17:32:57 +0500

    nothing changed

--- src/app/[locale]/products/page.tsx
+++ src/app/[locale]/products/page.tsx
@@ -1,19 +1,40 @@
+'use server'
+
+import { IProduct } from "@/types/product";
 import { unstable_setRequestLocale } from "next-intl/server";
+import { loadProducts } from '@/lib/load'
 import React from "react";
 
 type Props = {
     children: React.ReactNode
     params: {locale: string}
+    products: IProduct[]
 }
 
-export default function Products({children, params: {locale}}: Props) {
+
+export default function Products({children, params: {locale}, products}: Props) {
     unstable_setRequestLocale(locale);
 
     // const t = useTranslations('IndexPage');
 
     return (
         <>
-            Products Page
+            <ul>
+                {products.map((product) => (
+                    <li key={product.slug}>{product.name}</li>
+                ))}
+            </ul>
         </>
     );
+}
+
+ 
+// This function runs only on the server side
+export async function getStaticProps() {
+  // Instead of fetching your `/api` route you can call the same
+  // function directly in `getStaticProps`
+  const posts = await loadProducts()
+ 
+  // Props returned will be passed to the page component
+  return { props: { posts } }
 }
