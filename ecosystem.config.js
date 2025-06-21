c7e3f91e093aa3c003512e0fcb1ca34272f6166a
Cvdeby Coryphaeus <coryph13@gmail.com>
2025-06-21 16:29:57 +0500

    fix from server

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,26 +1,15 @@
 module.exports = {
-  apps: [
-    {
-      name: 'stw_app',
-      script: 'node_modules/.bin/next',
-      args: 'start -p 3003',
-      instances: 1,
-      autorestart: true,
-      max_memory_restart: '300M',
-      exec_mode: 'cluster',
-      env: {
-        NODE_ENV: 'production',
-        PORT: 3003
-      },
-      env_production: {
-        NODE_ENV: 'production',
-        PORT: 3003
-      },
+  apps: [{
+    name: "stw_app",
+    script: "npm",
+    args: "start",
+    env: {
+      PORT: 3003
+    },
       log_file: './logs/pm2.log',
       out_file: './logs/out.log',
       error_file: './logs/error.log',
       merge_logs: true,
       time: true
-    }
-  ]
+  }]
 };
